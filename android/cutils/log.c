3dfa02d1dfcffc265e471b7f49f7e4a309dad8ee
Jeff Kim <jeonghoe.kim@windriver.com>
2016-08-16 18:46:26 +0900

    Updated log system

--- android/cutils/log.c
+++ android/cutils/log.c
@@ -3,12 +3,25 @@
 
 #include "cutils/log.h"
 
+static const char * LOG_LEVEL[6] = {
+    "UNKNOWN", "DEFAULT", "VERBOSE", "DEBUG  ", "INFO   ", "WARN   ", "ERROR  ", "FATAL  ", "SILENT "
+};
+
+static int _default_log_level = ANDROID_LOG_WARN;
+
 int __android_log_print(int level, const char *tag, const char *fmt, ...)
 {
     va_list ap;
     int ret;
+    const char *ltag = LOG_LEVEL[0];
 
-    fprintf(stderr, "%s: ", tag);
+    if (level < _default_log_level)
+        return 0;
+
+    if (level > 0 && level <= ANDROID_LOG_SILENT)
+        ltag = LOG_LEVEL[level];
+
+    fprintf(stderr, "[%s] %s: ", ltag, tag);
     va_start(ap, fmt);
     ret = vfprintf(stderr, fmt, ap);
     va_end(ap);
@@ -33,5 +46,5 @@ void __android_log_assert(const char *cond, const char *tag, const char *fmt, ..
 int __android_log_is_loggable(int prio, const char *tag, int def)
 {
     int logLevel = def;
-    return logLevel >= 0 && prio >= logLevel;
+    return logLevel >= _default_log_level && prio >= logLevel;
 }
